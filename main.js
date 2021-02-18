98444c131ccee90c6883ea650251a1fa74e61144
H0rn0chse <h0rn0chse@gmx.de>
2021-02-19 00:43:34 +0100

    Updated websocket start order

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { startSocketServer } = require("./server/socket.js");
 const { startLobby } = require("./server/lobby.js");
 const { startGame } = require("./server/game.js");
 
-startFileServer();
 startSocketServer();
+startFileServer();
 startLobby();
 startGame();
