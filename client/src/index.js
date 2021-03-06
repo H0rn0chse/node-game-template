522bd5dbc69cd1ba6657d6ece6e3fa8c618f6222
H0rn0chse <h0rn0chse@gmx.de>
2021-03-06 01:06:40 +0100

    Added acknowledgements and licenses

--- client/src/index.js
+++ client/src/index.js
@@ -6,3 +6,9 @@ import { LobbyManager } from "./LobbyManager.js";
 openSocket().then(() => {
     LobbyManager.joinLobby();
 });
+
+const acknowledgements = document.querySelector("#acknowledgements");
+acknowledgements.innerHTML = globalThis.feather.icons.award.toSvg({ color: "#e2b007" });
+acknowledgements.addEventListener("click", (evt) => {
+    window.open("./acknowledgements/third-party-licenses.html", "_blank");
+}, { passive: true });
