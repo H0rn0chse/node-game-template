7d6d39b33a858803f3f323b595a32b4fac26fc4f
H0rn0chse <h0rn0chse@gmx.de>
2021-04-25 02:15:00 +0200

    Added EventBus

---
+++ client/src/EventBus.js
@@ -0,0 +1,82 @@
+class EventBus {
+    constructor (description = "") {
+        this.description = description;
+
+        this.eventHandler = new Map();
+    }
+
+    on (eventName, callback, scope) {
+        if (!this.has(eventName, callback, scope)) {
+            const key = {
+                eventName,
+                callback,
+                scope,
+            };
+
+            const value = {
+                once: false,
+                eventName,
+            };
+
+            this.eventHandler.set(key, value);
+        }
+    }
+
+    once (eventName, callback, scope) {
+        if (!this.has(eventName, callback, scope)) {
+            const key = {
+                eventName,
+                callback,
+                scope,
+            };
+
+            const value = {
+                once: true,
+                eventName,
+            };
+
+            this.eventHandler.set(key, value);
+        }
+    }
+
+    has (eventName, callback, scope) {
+        const key = {
+            eventName,
+            callback,
+            scope,
+        };
+        const value = this.eventHandler.get(key);
+        return !!value;
+    }
+
+    off (eventName, callback, scope) {
+        const key = {
+            eventName,
+            callback,
+            scope,
+        };
+        this.eventHandler.delete(key);
+    }
+
+    emit (eventName, ...args) {
+        this.eventHandler.forEach((value, key, map) => {
+            if (value.eventName !== eventName) {
+                return;
+            }
+
+            const boundHandler = key.scope ? key.callback.bind(key.scope) : key.callback;
+
+            if (value.once) {
+                map.delete(key);
+            }
+
+            try {
+                boundHandler(...args);
+            } catch (err) {
+                console.error(err);
+            }
+        });
+    }
+}
+
+export const GameBus = new EventBus("GameBus");
