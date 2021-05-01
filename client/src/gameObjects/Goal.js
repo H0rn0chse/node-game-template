e4abbb55e433dc2d149a1c0e02b6362f07c93453
H0rn0chse <h0rn0chse@gmx.de>
2021-05-01 23:23:14 +0200

    Replaced bouncy goal with tween

--- client/src/gameObjects/Goal.js
+++ client/src/gameObjects/Goal.js
@@ -1,18 +1,27 @@
-import { DYNAMIC, Phaser, PLAYER_STATUS } from "../globals.js";
+import { DYNAMIC, Phaser, PLAYER_STATUS, STATIC } from "../globals.js";
 import { GameManager } from "../views/GameManager.js";
 
 export class Goal extends Phaser.Physics.Arcade.Image {
-    constructor (scene, x, y) {
+    constructor (scene, x, initialY) {
         const { world } = scene.physics;
-        super(scene, x, y, "goal");
+        super(scene, x, initialY - 15, "goal");
 
         this.setScale(0.5);
         this.setOrigin(0, 1);
 
+        scene.tweens.add({
+            targets: this,
+            y: initialY + 10,
+            x,
+            ease: "Sine.easeInOut",
+            duration: 800,
+            yoyo: true,
+            repeat: -1,
+        });
+
         world.enable([this], DYNAMIC);
 
-        this.setBounceY(2);
-        this.setMaxVelocity(0, 100);
+        this.body.setAllowGravity(false);
 
         this.sound = scene.sound.add("win");
         scene.volume.addSound(this.sound);
